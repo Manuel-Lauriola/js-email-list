83721684acb194daa5adda564c21bbfbdf6509c1
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-18 16:50:53 +0200

    [UPD] - buttons

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 //dichiaro le variabili del mio html
 const commercial = document.getElementById(`commercial`)
 const support = document.getElementById(`support`)
+//per il bonus 2 prendo i 2 bottoni
+const newCommercial = document.getElementById(`new-commercial`)
+const newSupport = document.getElementById(`new-support`) 
 
 //definisco la funzione che genererà 5 mail casuali
 const randomMails = (destination) => {
@@ -12,5 +15,18 @@ const randomMails = (destination) => {
 }
 
 //invoco le funzioni
-randomMails(commercial)
 randomMails(support)
+randomMails(commercial)
+
+//aggiungo la funzione ai bottoni
+newSupport.addEventListener(`click`, (e) => {
+  e.preventDefault
+  support.innerHTML = ""
+  randomMails(support)
+})
+
+newCommercial.addEventListener(`click`, (e) => {
+  e.preventDefault
+  commercial.innerHTML = ""
+  randomMails(commercial)
+})
